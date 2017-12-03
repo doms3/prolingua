f268f51a01b5047d31fde4406ad5444404b50b62
Jeremy Wong <jeremywong@dhcp-206-87-153-187.ubcsecure.wireless.ubc.ca>
2017-12-02 16:37:26 -0800

    hardcoding ftw

--- src/main/java/prolingua/XProlinguaConverter.java
+++ src/main/java/prolingua/XProlinguaConverter.java
@@ -8,39 +8,52 @@ import java.util.Scanner;
 public class XProlinguaConverter {
 
 	public String convertToEnglProlingua(String xProl) throws FileNotFoundException {
-		//to return this string
+		// to return this string
 		String engl = "";
-		//to keep spacing between lines
+		// to keep spacing between lines
 		String spacing = "";
-		
+
 		Scanner sc = new Scanner(new FileInputStream(xProl));
-		
-		//check entire document, will take each line as a string, keeping spacing
-		while(sc.hasNextLine()) {
+
+		// check entire document, will take each line as a string, keeping spacing
+		while (sc.hasNextLine()) {
 
 			String currentLine = sc.nextLine();
-			
-			//eliminate and count the indents
-			while(currentLine.startsWith("[\t]")) {
+
+			// eliminate and count the indents
+			while (currentLine.startsWith("[\t]")) {
 				currentLine = currentLine.substring(1, currentLine.length());
 				engl += "\t";
 			}
-			
-			//analyze each word
+
+			// analyze each word
 			String[] words = currentLine.split("[\\s]");
-			
+
+			boolean hardCode = false;
 			// will return the English translation
 			for (String word : words) {
-				engl += JsonConverter.getInstance().getTranslation(word) + " ";
+				String nextWord = JsonConverter.getInstance().getTranslation(word) + " ";
+				
+				if((hardCode)&&(nextWord.equals("is"))){
+					engl += "is not ";
+					hardCode = false;
+				}
+				else if( (hardCode)&&(!(nextWord.equals("is")))) {
+					engl += "not " + nextWord + " ";
+				}
+				else if (nextWord.equals("not")) {
+					hardCode = true;
+				}
+				else if(!hardCode){
+					engl += nextWord + " ";
+				}
 			}
-			
+
 			engl += "\n";
-			
+
 		}
-		
-		
-		
+
 		return engl;
 	}
-	
+
 }
